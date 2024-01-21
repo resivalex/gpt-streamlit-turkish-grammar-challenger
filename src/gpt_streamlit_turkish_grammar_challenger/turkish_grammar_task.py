1a9726fb662739cf7d46d428ef2f276f9d7bbbe5
Ivan Reshetnikov <resivalex@gmail.com>
2024-01-21 19:01:22 +0600

    Generate task

--- src/gpt_streamlit_turkish_grammar_challenger/turkish_grammar_task.py
+++ src/gpt_streamlit_turkish_grammar_challenger/turkish_grammar_task.py
@@ -21,56 +21,59 @@ class TurkishGrammarTask:
         "Causative Form",
         "Negative Form",
     ]
-    PROMPT = """
-You are Assistant, tailored for Russian speakers learning Turkish grammar, emphasizes interactive learning through an example-driven approach. It communicates in Russian and mirrors the provided interaction example format.
+    PROMPT = """Task:
+1. You are provided with a vocabulary topic and a grammar rule.
+2. Present a Turkish phrase with its Russian translation and two additional Turkish phrases. These phrases are designed to help Russian speakers understand Turkish grammar by illustrating the correct use of the selected rule and challenging them with similar phrases.
 
-Assistant Overview:
-- Focus: Comprehensive exploration of Turkish grammatical rules such as tenses, adjectives, participles, pronouns, gerunds, etc.
-- Method: Progressive introduction of grammatical concepts, focusing on compound words in Turkish, without initially revealing the grammar rule.
+Objective:
+- To facilitate the understanding of Turkish grammatical rules for Russian speakers through practical, interactive examples.
+- To emphasize learning through example-driven approach, focusing on grammar nuances and vocabulary in context.
+- To aid in distinguishing correct grammatical forms from similar but incorrect ones, enhancing grammar learning.
 
-Task:
-1. User Theme Selection: The user selects a vocabulary topic (for words, not grammar) to create Russian sentences.
-2. Grammar Rule Selection: The user selects a grammar rule.
-3. Task Presentation: The Assistant presents a Turkish phrase highlighting a provided Turkish grammar rule without disclosing the rule, followed by Russian phrase and two Turkish phrase options.
-4. Output format: Follow format descriptions below without adding any extra elements.
+Content Requirements:
+1. Phrase Clarity: Use brief, clear phrases in Turkish to ensure understanding.
+2. Grammar Emphasis: Highlight the specific Turkish grammar rule in each option, focusing on nuances like suffixes and word order.
+3. Vocabulary: Utilize common, understandable vocabulary that is relevant to the selected topic.
 
-Turkish phrase: "[Turkish phrase]"
+Translation Options:
+1. Translation Variation: Provide three Turkish translations, each reflecting the specified grammar rule but varying in grammatical forms.
+2. Structure Consistency: Maintain similar phrase structures in all translations to challenge correct identification.
+3. Accuracy: Ensure grammatical correctness in the main Turkish option, with variations in the challenging phrases.
+
+Input format:
+
+Grammar rule: "[Grammar rule]"
+Vocabulary topic: "[Vocabulary topic]"
+
+Output format:
+
+Grammar rule: "[Grammar rule]"
+Vocabulary topic: "[Vocabulary topic]"
+Correct Turkish phrase: "[Turkish phrase]"
 Russian translation: "[Russian phrase]"
 First challenging Turkish phrase: "[Similar Turkish phrase 1]"
 Second challenging Turkish phrase: "[Similar Turkish phrase 2]"
 
-
-Task Instructions:
-- Initiate a task with a Turkish phrase, Russian translation and two Turkish translation options.
-- Refrain from extra comments or instructions during the task.
-
-Content Requirements:
-1. Phrase Clarity: Use brief, clear phrases in Turkish.
-2. Grammar Emphasis: Highlight the specific Turkish grammar rule in each option.
-3. Vocabulary: Utilize common, understandable vocabulary, focusing on Turkish grammar nuances, especially suffixes and word order.
-
-Translation Options:
-1. Translation Variation: Provide three numbered Turkish translations, each reflecting the specified grammar rule.
-2. Structure Consistency: Maintain similar phrase structures in all translations to challenge correct identification.
-3. Focus on Grammar: Vary translations through grammatical forms, not vocabulary.
-4. Accuracy: Ensure grammatical correctness in all Turkish options.
-
-Objective:
-1. Facilitate understanding of grammatical rules in word formation.
-2. Use simple, everyday words to highlight grammar learning.
-
 Example Task:
 
-Turkish phrase: "Kitap okuyorum"
+Input:
+
+Grammar rule: "Present Continuous Tense"
+Vocabulary topic: "Досуг"
+
+Output:
+
+Grammar rule: "Present Continuous Tense"
+Vocabulary topic: "Досуг"
+Correct Turkish phrase: "Kitap okuyorum"
 Russian translation: "Я читаю книгу"
 First challenging Turkish phrase: "Kitabı okurum"
 Second challenging Turkish phrase: "Kitap okur"
 
+Proceed with the next task:
 
-User Theme: "{topic}"
-Grammar Rule: "{rule}"
-
-Create a task.
+Grammar rule: "{rule}"
+Vocabulary topic: "{topic}"
 """
 
     def __init__(self, vocabulary_topic: str, api_client: OpenAiApiClient):
@@ -92,12 +95,16 @@ Create a task.
             schema={
                 "type": "object",
                 "properties": {
+                    "grammar_rule": {"type": "string"},
+                    "vocabulary_topic": {"type": "string"},
                     "turkish_phrase": {"type": "string"},
                     "russian_translation": {"type": "string"},
                     "first_challenging_turkish_phrase": {"type": "string"},
                     "second_challenging_turkish_phrase": {"type": "string"},
                 },
                 "required": [
+                    "grammar_rule",
+                    "vocabulary_topic",
                     "turkish_phrase",
                     "russian_translation",
                     "first_challenging_turkish_phrase",
